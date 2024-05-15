17783bef07a5dc67546ab34349260b52313adbfa
justagit07 <droptelegram@outlook.com>
2024-05-15 18:11:59 +0530

    installed the dev dependency nodemon

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+// dotenv for the unshareable keys
 
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+
+
 
 const app= express();
 app.use(express.json());
@@ -28,4 +31,12 @@ const  dbconnection = ()=>
 
  dbconnection();
 
- 
+
+// creating server of express
+
+
+app.listen(process.env.PORT || 3001, ()=>
+    {
+        console.log(`your server is running at the 3000 \n http://localhost:${process.env.PORT}`);
+    })
+    
